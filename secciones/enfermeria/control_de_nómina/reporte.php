7a44ade24f13a6b5e80d1807ed2bb6b327006789
Jonatan Bonilla <jonatanbonsosa@gmail.com>
2023-09-12 07:57:34 -0600

    borre archivos y modifique el archivo reporte.php

--- secciones/enfermeria/control_de_nómina/reporte.php
+++ secciones/enfermeria/control_de_nómina/reporte.php
@@ -1,44 +1,52 @@
 <?php
 
-include ("../../../connection/conexion.php");
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
+include("../../../connection/conexion.php");
+include("../../../vendor/autoload.php");
+
+use PhpOffice\PhpSpreadsheet\{Spreadsheet, IOFactory};
 
 
-$sql = $con->query("SELECT trabajador.*, puestos.Nombre_puestos 
-FROM usuarios AS trabajador 
-JOIN estado AS est ON trabajador.Estado = est.id_estado
-JOIN puestos ON trabajador.id_departamentos = puestos.id_puestos
-WHERE Nombre_puestos = 'Enfermeria'");
-$sql = $con->query($sql);
+$consulta = "
+    SELECT trabajador.*, puestos.Nombre_puestos 
+    FROM usuarios AS trabajador 
+    JOIN estado AS est ON trabajador.Estado = est.id_estado
+    JOIN puestos ON trabajador.id_departamentos = puestos.id_puestos
+    WHERE Nombre_puestos = 'Enfermeria'
+";
 
-$excel = new  Spreadsheet();
+$sentencia = $con->prepare($consulta);
+$sentencia->execute();
+$trabajadores = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+
+$excel = new Spreadsheet();
 $hojaActiva = $excel->getActiveSheet();
 $hojaActiva->setTitle("Nomina");
 
-
-$hojaActiva->setCellValue('A1','Usuario');
-
-$hojaActiva->setCellValue('B1','Nombre');
-
-$hojaActiva->setCellValue('C1','Apellidos');
-
-$hojaActiva->setCellValue('D1','Dias laborados');
-
-$hojaActiva->setCellValue('F1','Sueldo total');
-
-$hojaActiva->setCellValue('G1','Tipo de guardia');
+$hojaActiva->setCellValue('A1', 'Usuario');
+$hojaActiva->setCellValue('B1', 'Nombre');
+$hojaActiva->setCellValue('C1', 'Apellidos');
+$hojaActiva->setCellValue('D1', 'Puesto'); // Cambié 'Dias laborados' a 'Puesto' ya que no se encuentra en tu consulta SQL
+$hojaActiva->setCellValue('E1', 'Código Postal'); // Agregué 'Código Postal' como una columna adicional
+$hojaActiva->setCellValue('F1', 'RFC'); // Agregué 'RFC' como una columna adicional
 
 $fila = 2;
 
-while ($rows = $sql->PDO::fetch_assoc($resultado)) {
-$hojaActiva->setCellValue('A'.$fila, $rows['id_usuarios']);
-$hojaActiva->setCellValue('A'.$fila, $rows['Nombres']);
-$hojaActiva->setCellValue('A'.$fila, $rows['Apellidos']);
-$hojaActiva->setCellValue('A'.$fila, $rows['Nombre_puestos']);
-$hojaActiva->setCellValue('A'.$fila, $rows['codigo_postal']);
-$hojaActiva->setCellValue('A'.$fila, $rows['rfc']);	
-$fila++;
+foreach ($trabajadores as $row) {
+    $hojaActiva->setCellValue('A' . $fila, $row['id_usuarios']);
+    $hojaActiva->setCellValue('B' . $fila, $row['Nombres']);
+    $hojaActiva->setCellValue('C' . $fila, $row['Apellidos']);
+    $hojaActiva->setCellValue('D' . $fila, $row['Nombre_puestos']);
+    $hojaActiva->setCellValue('E' . $fila, $row['codigo_postal']);
+    $hojaActiva->setCellValue('F' . $fila, $row['rfc']);
+    $fila++;
 }
 
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment;filename="nomina.xlsx"');
+header('Cache-Control: max-age=0');
+
+$writer = IOFactory::createWriter($excel, 'Xlsx');
+$writer->save('php://output');
 
 ?>
