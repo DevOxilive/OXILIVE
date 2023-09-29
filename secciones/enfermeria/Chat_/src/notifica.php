c2a6add72e6a6079a26bb51d251f622de9bc41a9
Oscar <oslu3132@gmail.com>
2023-09-29 10:02:10 -0600

    notify exelent very home XD

--- secciones/enfermeria/Chat_/src/notifica.php
+++ secciones/enfermeria/Chat_/src/notifica.php
@@ -9,24 +9,7 @@ try {
     $sent = $con->prepare("SELECT * FROM mensajes WHERE id_salida = {$_SESSION['idus']} AND leido = {$estado}");
     $sent->execute();
     $result = $sent->fetchAll(PDO::FETCH_ASSOC);
-
-
     echo '<i class="bi bi-bell"></i> Mensajes sin leer: ' . count($result);
-    // if (count($result) > 0) {
-    //     foreach($result as $filas){
-    //         echo '<script>
-    //         Push.create("'. $filas['persona'] .'", {
-    //             body: "'. $filas['msg'] .'",
-    //             icon: "img/usuario.png",
-    //             onClick: function() {
-    //                 window.focus();
-    //                 this.close();
-    //             }
-    //         });
-    //         </script>';
-    //     }
-    // }
-    // si deseas que una alerta molesta, quita los comentarios
 } catch (Exception $e) {
     $e->getMessage();
 }
