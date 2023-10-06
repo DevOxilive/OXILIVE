8b66b449110a57d68d4dcb8da367d894c87e0032
Oscar <oslu3132@gmail.com>
2023-10-06 13:08:20 -0600

    rsastro de token's, gestion de condicionales.

--- secciones/enfermeria/Chat_/probar.php
+++ secciones/enfermeria/Chat_/probar.php
@@ -1,21 +1,35 @@
-<?php
-include '../../../connection/conexion.php';
+<!DOCTYPE html>
+<html lang="en">
 
-do {
-    $regresar = false;
-    $token = bin2hex(random_bytes(32));
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>token comprueba</title>
+</head>
 
-    $checkToken = $con->prepare("SELECT token FROM usuarios");
-    $checkToken->execute();
-    $existToken = $checkToken->fetchAll(PDO::FETCH_ASSOC);
-    foreach ($existToken as $tokenCheck) {
-        if ($tokenCheck == $token) {
-            $regresar = true;
-            echo "son iguales";
-        } else {
-            $token = bin2hex(random_bytes(32));
-            $regresar = false;
+<body>
+    <?php
+    include '../../../connection/conexion.php';
+
+    do {
+        $regresar = false;
+        $token = bin2hex(random_bytes(32));
+
+        $checkToken = $con->prepare("SELECT token FROM usuarios");
+        $checkToken->execute();
+        $existToken = $checkToken->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($existToken as $tokenCheck) {
+            if ($tokenCheck == $token) {
+                $regresar = true;
+                echo "son iguales";
+            } else {
+                $token = bin2hex(random_bytes(32));
+                $regresar = false;
+            }
         }
-    }
-    echo "este es nuevo: {$token} <br>";
-} while ($regresar == true);
+        echo "<h1>este es nuevo:</h1><p>{$token} </p><br>";
+    } while ($regresar == true);
+    ?>
+</body>
+
+</html>
