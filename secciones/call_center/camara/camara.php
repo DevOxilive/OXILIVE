1e19fc0a9c6a6d556f39cd5a0684cd8aa1060e2f
DevOxilive <sistemas@oxilive.com.mx>
2023-10-18 09:49:22 -0600

    Implementación de modulo call center medicos y camara así como servicios

--- secciones/call_center/camara/camara.php
+++ secciones/call_center/camara/camara.php
@@ -1,105 +1,98 @@
+<?php
+session_start();
+if (!isset($_SESSION['us'])) {
+    header('Location: ../../../../login.php');
+} elseif (isset($_SESSION['us'])) {
+    include("../../../templates/header.php");
+    include("../../../connection/conexion.php");
+} else {
+    echo "Error en el sistema";
+}
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Scanner</title>
+    <link rel="stylesheet" href="<?php echo $url_base; ?>assets/css/foto_perfil.css">
+    <link rel="stylesheet" href="<?php echo $url_base; ?>assets/css/edit.css">
     <style>
-        /* Estilos para centrar el contenido y hacerlo responsivo */
-        body {
-            background-color: rgb(0, 0, 49) !important;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-            margin: 0;
-        }
-        h1{
-            color: #fff !important;
-        }
-        #canvasContainer {
-            display: flex; /* Hacer que los elementos se muestren en línea */
-            align-items: center; /* Centrar verticalmente */
-            justify-content: center; /* Centrar horizontalmente */
-        }
+    #videoContainer {
+        position: absolute;
+        top: 6.5vh;
+        left: 3.5vh;
+    }
 
-        #canvas {
-            max-width: 100%;
-            height: auto;
-        }
-
-        #capturedImage {
-            max-width: 240px; /* Puedes ajustar el tamaño máximo según tus necesidades */
-            display: none;
-        }
-
-        #videoContainer {
-            position: absolute;
-            top: 18vh;
-            left: 62vh;
-        }
-
-        #video {
-            width: 100px; /* Ancho reducido a 20px */
-            height: auto;
-        }
-
-        /* Estilos para los botones */
-        .btn-container {
-            display: flex;
-            gap: 10px;
-            margin-top: 5px;
-        }
+    #video {
+        width: 100px;
+        height: auto;
+    }
     </style>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body>
-    <h1>Captura el INE</h1>
-    <div id="canvasContainer">
-        <canvas id="canvas"></canvas>
-        <div id="videoContainer">
-            <video id="video" autoplay></video>
+<main id="main" class="main">
+
+    <body>
+        <div class="container">
+            <h1 class="text-center mt-4">Captura Tu Reporte</h1>
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div id="canvasContainer" class="text-center" style="margin-top: 40px;">
+                                <canvas id="canvas" class="img-fluid"></canvas>
+                                <div id="videoContainer">
+                                    <video id="video" autoplay class="img-fluid"></video>
+                                </div>
+                                <img id="capturedImage" class="img-fluid">
+                            </div>
+                            <div class="btn-container text-center">
+                                <button type="button" class="btn btn-success btn-sm" id="startCamera">Iniciar
+                                    Cámara</button>
+                                <button type="button" class="btn btn-danger btn-sm" id="stopDetection" disabled>Detener
+                                    Detección</button>
+
+                                <button type="button" class="btn btn-info btn-sm" id="capture" style="display:none;"
+                                    disabled>Capturar</button>
+                                <button type="button" class="btn btn-primary btn-sm" id="showImage"
+                                    style="display:none;" disabled>Descargar
+                                    PDF</button>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <img id="capturedImage"> <!-- Imagen capturada al lado de la canvas -->
-    </div>
-    <div class="btn-container">
-        <button type="button" class="btn btn-success" id="startCamera">Iniciar Cámara</button>
-        <button type="button" class="btn btn-danger" id="stopDetection" disabled>Detener Detección</button>
-        <button type="button" class="btn btn-info" id="capture" disabled>Capturar</button>
-        <button type="button" class="btn btn-primary" id="showImage" disabled>Descargar PDF</button>
-    </div>
-    
-    <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="https://html2canvas.hertzen.com/dist/html2canvas.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/pdf-lib@1.9.0/dist/pdf-lib.js"></script>
-
-
-    <script>
+        <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+        </script>
+        <script src="https://html2canvas.hertzen.com/dist/html2canvas.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/pdf-lib@1.9.0/dist/pdf-lib.js"></script>
+        <script>
         let video, canvas, context, model, stream, intervalId, closestPrediction;
-
         async function setupCamera() {
             video = document.getElementById('video');
             canvas = document.getElementById('canvas');
             context = canvas.getContext('2d');
-            stream = await navigator.mediaDevices.getUserMedia({ video: true });
+            stream = await navigator.mediaDevices.getUserMedia({
+                video: true
+            });
             video.srcObject = stream;
             video.onloadedmetadata = () => {
                 canvas.width = video.videoWidth;
                 canvas.height = video.videoHeight;
-                runObjectDetection(); // Mueve esta llamada aquí
+                runObjectDetection();
+                document.getElementById('stopDetection').disabled =
+                    false;
             };
         }
-
         async function runObjectDetection() {
             model = await cocoSsd.load();
-
             intervalId = setInterval(async () => {
                 context.clearRect(0, 0, canvas.width, canvas.height);
                 context.drawImage(video, 0, 0);
                 const predictions = await model.detect(video);
-
                 if (predictions.length > 0) {
                     closestPrediction = predictions[0];
                     context.beginPath();
@@ -113,109 +106,137 @@
                 }
             }, 100);
         }
-
-        // Obtener referencias a los botones
         const startCameraButton = document.getElementById('startCamera');
         const stopDetectionButton = document.getElementById('stopDetection');
         const captureButton = document.getElementById('capture');
-        const capturedImage = document.getElementById('capturedImage');
-        const showImageButton = document.getElementById('showImage'); // Botón para mostrar la imagen
-
+        const showImageButton = document.getElementById('showImage');
         startCameraButton.addEventListener('click', async () => {
             if (!stream) {
                 await setupCamera();
-                startCameraButton.disabled = true;
-                stopDetectionButton.disabled = false;
+                startCameraButton.style.display = 'none';
+                stopDetectionButton.style.display = 'inline';
+                captureButton.style.display = 'inline';
+                showImageButton.style.display = 'inline';
             }
         });
-
         stopDetectionButton.addEventListener('click', () => {
             if (stream) {
                 stream.getTracks().forEach(track => track.stop());
                 clearInterval(intervalId);
                 context.clearRect(0, 0, canvas.width, canvas.height);
-                startCameraButton.disabled = false;
-                stopDetectionButton.disabled = true;
-                captureButton.disabled = true;
-                capturedImage.style.display = 'none';
-                showImageButton.disabled = false;
+                startCameraButton.style.display = 'inline';
+                stopDetectionButton.style.display = 'none';
+                captureButton.style.display = 'none';
+                showImageButton.style.display = 'none';
                 location.reload();
             }
         });
 
         captureButton.addEventListener('click', () => {
-        // Tomar una captura de la detección
-        if (closestPrediction) {
-            const x = Math.max(0, closestPrediction.bbox[0]);
-            const y = Math.max(0, closestPrediction.bbox[1]);
-            const width = Math.min(canvas.width - x, closestPrediction.bbox[2]);
-            const height = Math.min(canvas.height - y, closestPrediction.bbox[3]);
+            // Aquí voy a tomar mi captura de la canva
+            if (closestPrediction) {
+                const x = Math.max(0, closestPrediction.bbox[0]);
+                const y = Math.max(0, closestPrediction.bbox[1]);
+                const width = Math.min(canvas.width - x, closestPrediction.bbox[2]);
+                const height = Math.min(canvas.height - y, closestPrediction.bbox[3]);
 
-            const captureCanvas = document.createElement('canvas');
-            captureCanvas.width = width;
-            captureCanvas.height = height;
-            const captureContext = captureCanvas.getContext('2d');
-            captureContext.drawImage(video, x, y, width, height, 0, 0, width, height);
+                // Crear un nuevo canvas para la captura
+                const captureCanvas = document.createElement('canvas');
+                captureCanvas.width = width;
+                captureCanvas.height = height;
+                const captureContext = captureCanvas.getContext('2d');
 
-            // Mostrar la captura al lado
-            capturedImage.src = captureCanvas.toDataURL('image/png');
-            capturedImage.style.display = 'block';
-        }
-    });
+                // Capturar la parte completa de la detección
+                captureContext.drawImage(video, x, y, width, height, 0, 0, width, height);
 
-    showImageButton.addEventListener('click', async () => {
-            // Verificar si la imagen capturada existe
+                // Mostrar la captura al lado
+                capturedImage.src = captureCanvas.toDataURL('image/jpeg');
+                capturedImage.style.display = 'block';
+
+                // Agregar la captura al PDF
+                const imgData = captureCanvas.toDataURL('image/jpeg');
+                const {
+                    PDFDocument,
+                    rgb
+                } = 'PDFLib';
+
+                (async () => {
+                    const pdfDoc = await PDFDocument.create();
+                    const page = pdfDoc.addPage([width, height]);
+                    const jpgImage = await pdfDoc.embedJpg(imgData);
+                    const imgDims = jpgImage.scale(1);
+                    page.drawImage(jpgImage, {
+                        x: 0,
+                        y: 0,
+                        width: imgDims.width,
+                        height: imgDims.height,
+                        color: rgb(0, 0, 0),
+                    });
+                    const pdfBytes = await pdfDoc.save();
+                    const pdfBlob = new Blob([pdfBytes], {
+                        type: 'application/pdf'
+                    });
+                    const pdfUrl = URL.createObjectURL(pdfBlob);
+
+                    const link = document.createElement('a');
+                    link.href = pdfUrl;
+                    link.download = 'Expediente.pdf';
+                    link.style.display = 'none';
+                    document.body.appendChild(link);
+                    link.click();
+                    document.body.removeChild(link);
+                    URL.revokeObjectURL(pdfUrl);
+                })();
+            }
+        });
+
+        showImageButton.addEventListener('click', async () => {
             if (capturedImage.src) {
                 const imgData = capturedImage.src;
-
                 // Crear un nuevo PDF
-                const { PDFDocument, rgb } = PDFLib;
-
+                const {
+                    PDFDocument,
+                    rgb
+                } = PDFLib;
                 const pdfDoc = await PDFDocument.create();
-                const page = pdfDoc.addPage([100, 100]);
-                const { width, height } = page.getSize();
-
-                // Cargar la imagen y agregarla a la página
+                const page = pdfDoc.addPage([canvas.width, canvas.height]);
+                // Agregar la imagen y agregarla a la página pdf
                 const jpgImage = await pdfDoc.embedJpg(imgData);
                 const jpgDims = jpgImage.scale(1);
-
-                const imgWidth = jpgDims.width;
-                const imgHeight = jpgDims.height;
-
-                const x = (width - imgWidth) / 2;
-                const y = (height - imgHeight) / 2;
-
                 page.drawImage(jpgImage, {
-                    x,
-                    y,
-                    width: imgWidth,
-                    height: imgHeight,
+                    x: 0,
+                    y: 0,
+                    width: jpgDims.width,
+                    height: jpgDims.height,
                     color: rgb(0, 0, 0),
                 });
-
                 // Generar el archivo PDF
                 const pdfBytes = await pdfDoc.save();
-
                 // Crear un objeto Blob para el archivo PDF
-                const pdfBlob = new Blob([pdfBytes], { type: 'application/pdf' });
-
+                const pdfBlob = new Blob([pdfBytes], {
+                    type: 'application/pdf'
+                });
                 // Crear un enlace para descargar el PDF
                 const pdfUrl = URL.createObjectURL(pdfBlob);
                 const link = document.createElement('a');
                 link.href = pdfUrl;
-                link.download = 'captura.pdf';
+                link.download = 'Expediente.pdf';
                 link.style.display = 'none';
-
                 // Agregar el enlace al cuerpo del documento y hacer clic para descargar
                 document.body.appendChild(link);
                 link.click();
-
                 // Limpiar después de la descarga
                 document.body.removeChild(link);
                 URL.revokeObjectURL(pdfUrl);
             }
         });
+        </script>
+        <script src="js/nextService.js"></script>
+        <script src="js/rangeActivity.js"></script>
+    </body>
 
-    </script>
-</body>
 </html>
+</main>
+<?php
+include("../../../templates/footer.php");
+?>
--- secciones/call_center/medicos/index.php
+++ secciones/call_center/medicos/index.php
@@ -5,11 +5,13 @@ if (!isset($_SESSION['us'])) {
 }elseif (isset($_SESSION['us'])) {
     include("../../../templates/header.php");
     include("../../../connection/conexion.php");
+    include("consulta.php");
 }else{
     echo "Error en el sistema";
 }
 ?>
 <!DOCTYPE html>
+
 <head>
     <link rel="stylesheet" href="<?php echo $url_base; ?>assets/css/foto_perfil.css">
     <link rel="stylesheet" href="<?php echo $url_base; ?>assets/css/edit.css">
@@ -26,100 +28,103 @@ if (!isset($_SESSION['us'])) {
     <section class="section dashboard">
         <div class="row">
             <!-- Left side columns -->
-            <div class="col-lg-8">
-                <div class="row">
-                    <!-- Card Proximo Servicio -->
-                    <div class="col-12">
-                        <div class="card info-card next-service-card">
-                            <div class="card-body">
-                                <h5 class="card-title"></h5>
-                                <!-- <?php if (!empty($lista_servicios)) { ?>
-                                    <?php foreach ($lista_servicios as $servicios) { ?> -->
-                                        <div class="d-flex align-items-center">
-                                            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                                <i class="bi bi-clipboard-pulse"></i>
-                                            </div>
-                                            <div class="ps-4 ">
-                                                <h6><?php echo $servicios['nomPaciente']; ?></h6>
-                                                <span class="text-muted small pt-2 ps-1"><?php echo $servicios['nomGuardia']; ?></span>
-                                            </div>
-                                        </div>
-                                        <br>
-                                        <!-- Botón de Empezar/Terminar Servicio -->
-                                        <div class="ps-4" id="service-button"></div>
-                                        <input type="hidden" id="idHor" name="idHor" value="<?php echo $servicios['id_asignacionHorarios'] ?>">
-                                    <!-- <?php } ?>
-                                <?php } else { ?> -->
-                                    <div class="d-flex align-items-center">
-                                        <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                                            <i class="bi bi-emoji-frown"></i>
-                                        </div>
-                                        <div class="ps-4 ">
-                                            <h6>¡Lo siento!</h6>
-                                            <span class="text-muted small pt-2 ps-1">No tienes próximos servicios</span>
-                                        </div>
-                                    </div>
-                                <?php } ?>
-                            </div>
-                        </div>
-                    </div><!-- End Card Próximo Servicio -->
-                    <!-- Card Actividad reciente -->
-                    <div class="col-12">
+            <div class="col-12">
+                <div class="card info-card next-service-card">
+                    <div class="card-body">
+                        <!--Aquí empiezo mis pruebas xD-->
+                        <?php foreach ($ltServicio as $servicio) : ?>
                         <div class="card">
-                            <div class="filter">
-                                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                                    <li class="dropdown-header text-start">
-                                        <h6>Filtro</h6>
-                                    </li>
-
-                                    <li><a class="dropdown-item" onclick="range(this)">Hoy</a></li>
-                                    <li><a class="dropdown-item" onclick="range(this)">Esta semana</a></li>
-                                    <li><a class="dropdown-item" onclick="range(this)">Esta quincena</a></li>
-                                </ul>
-                            </div>
-
                             <div class="card-body">
-                                <h5 class="card-title">Actividad Reciente <span>| </span><span id="range-activity">Hoy</span></h5>
-
-                                <div class="activity" id="activity">
-
+                                <h5 class="card-title">Solicitado por: <?php echo $servicio['nom_solicitante']; ?></h5>
+                                <p class="card-text">Motivo de consulta: <?php echo $servicio['moti_consulta']; ?></p>
+                                <div class="map-container">
+                                    <!-- <iframe
+                                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1dLATITUD!2dLONGITUD!3d15!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTXCsDQzJzQ1LjEiTiAxNcKwMzUnMTkuMCJX!5e0!3m2!1sen!2sus!4v1622145678145!5m2!1sen!2sus"
+                                                width="600" height="450" style="border:0;" allowfullscreen=""
+                                                loading="lazy"></iframe> -->
                                 </div>
-
+                                <!-- Botón "Iniciar Servicio" -->
+                                <?php
+                                    $asignacion = $servicio['asignacion'];
+                                    if ($asignacion == 1) {
+                                    $mensaje = "Servicio Iniciado";
+                                    } else {
+                                    $mensaje = "Iniciar Servicio";
+                                    }
+                                ?>
+                                <a href="#" class="btn btn-primary" onclick="mostrarAlerta('<?php echo $mensaje; ?>')"><?php echo $mensaje; ?></a>
                             </div>
                         </div>
-                    </div><!-- End Actividad reciente -->
-                    <!-- Horarios del usuario -->
-                    <div class="col-12">
-                        <div class="card info-card customers-card">
-                            <!-- Filtro -->
-                            <div class="filter">
-                                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                                    <li class="dropdown-header text-start">
-                                        <h6>Filter</h6>
-                                    </li>
-                                    <li><a class="dropdown-item" href="#">Today</a></li>
-                                    <li><a class="dropdown-item" href="#">This Month</a></li>
-                                    <li><a class="dropdown-item" href="#">This Year</a></li>
-                                </ul>
-                            </div>
-                            <!-- End Filtro -->
-                            <div class="card-body">
-                                <h5 class="card-title">Horarios <span>| Últimos tres</span></h5>
-
-                            </div>
-                        </div>
-                    </div><!-- End Horarios del usuario -->
-                    
+                        <?php endforeach; ?>
+                        <!--Aquí termina mi card de iniciar el servicio-->
+                    </div>
                 </div>
             </div>
-            <!-- End Left side columns -->
+            <!--Este es su modelo de alex End Card Próximo Servicio -->
+            <!-- Card Actividad reciente -->
+            <div class="col-12">
+                <div class="card">
+                    <div class="filter">
+                        <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                            <li class="dropdown-header text-start">
+                                <h6>Filtro</h6>
+                            </li>
+
+                            <li><a class="dropdown-item" onclick="range(this)">Hoy</a></li>
+                            <li><a class="dropdown-item" onclick="range(this)">Esta semana</a></li>
+                            <li><a class="dropdown-item" onclick="range(this)">Esta quincena</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="card-body">
+                        <h5 class="card-title">Actividad Reciente <span>| </span><span id="range-activity">Hoy</span>
+                        </h5>
+
+                        <div class="activity" id="activity">
+
+                        </div>
+
+                    </div>
+                </div>
+            </div><!-- End Actividad reciente -->
+            <!-- Horarios del usuario -->
+            <div class="col-12">
+                <div class="card info-card customers-card">
+                    <!-- Filtro -->
+                    <div class="filter">
+                        <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                            <li class="dropdown-header text-start">
+                                <h6>Filter</h6>
+                            </li>
+                            <li><a class="dropdown-item" href="#">Today</a></li>
+                            <li><a class="dropdown-item" href="#">This Month</a></li>
+                            <li><a class="dropdown-item" href="#">This Year</a></li>
+                        </ul>
+                    </div>
+                    <!-- End Filtro -->
+                    <div class="card-body">
+                        <h5 class="card-title">Horarios <span>| Últimos tres</span></h5>
+
+                    </div>
+                </div>
+            </div><!-- End Horarios del usuario -->
+
+        </div>
+        </div>
+        <!-- End Left side columns -->
         </div>
     </section>
 </main>
 
 </html>
+
+<script>
+    function mostrarAlerta(mensaje) {
+        alert(mensaje);
+    }
+</script>
 <script src="js/nextService.js"></script>
 <script src="js/rangeActivity.js"></script>
 <?php
