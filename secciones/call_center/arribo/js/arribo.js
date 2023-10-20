7ecef6f992fe17a7bbb194bca830fe807bb174bc
Oscar <oslu3132@gmail.com>
2023-10-20 15:06:13 -0600

    arribo final

--- secciones/call_center/arribo/js/arribo.js
+++ secciones/call_center/arribo/js/arribo.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-    
+
+    function arribo() {
+        $.ajax({
+            url: 'php/cargarArribo.php',
+            type: 'POST',
+            success: function (data) {
+                $('#mostrarArribo').html(data);
+            }
+        });
+    }
+
+    setInterval(() => {
+        arribo();
+    }, 1000);
 });
