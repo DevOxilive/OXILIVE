4ddb84000d6911f2b2d180ee63515254e0008ceb
Alex Venegas <alfredo.alexis30@gmail.com>
2023-10-24 16:57:27 -0600

    Update index Pacientes Call Center
    No deja eliminar por problemas con la class clickeable

--- secciones/call_center/pacientes/index.php
+++ secciones/call_center/pacientes/index.php
@@ -14,18 +14,20 @@ if (!isset($_SESSION['us'])) {
 <html lang="es">
 
 <head>
-    <link rel="stylesheet" href="../../../../assets/css/foto_perfil.css">
-    <link rel="stylesheet" href="../../../../assets/css/edit.css">
+    <link rel="stylesheet" href="../../../assets/css/foto_perfil.css">
+    <link rel="stylesheet" href="../../../assets/css/edit.css">
 </head>
 <main id="main" class="main">
     <section class="section dashboard">
         <div class="card">
             <div class="card-header">
                 <h2>Pacientes</h2>
-                <a class="btn btn-outline-primary" href="pages/crearPaciente.php" role="button">
-                    <i class="bi bi-calendar-plus"></i>
-                    Nuevo Paciente
-                </a>
+                <?php if ($_SESSION['puesto'] == 1 || $_SESSION['puesto'] == 5) { ?>
+                    <a class="btn btn-outline-primary" href="pages/crearPaciente.php" role="button">
+                        <i class="bi bi-calendar-plus"></i>
+                        Nuevo Paciente
+                    </a>
+                <?php } ?>
             </div>
             <div class="card-body">
                 <div class="table-responsive-sm">
@@ -63,11 +65,11 @@ if (!isset($_SESSION['us'])) {
                                     <?php if ($_SESSION['puesto'] == 1 || $_SESSION['puesto'] == 5) { ?>
                                         <td>
                                             <center>
-                                                <a name="" id="" class="btn btn-outline-warning" href="pages/editarPaciente.php?idPac=<?php echo $datos['id_pacientes']; ?>" role="button">
-                                                    <i class="bi bi-pencil-square"></i>
+                                                <a class="btn btn-outline-primary" href="pages/editarPaciente.php?idPac=<?php echo $datos['id_pacientes']; ?>" role="button">
+                                                    <i class="bi bi-pencil-square"></i><span> Editar</span>
                                                 </a> |
-                                                <a name="" class="btn btn-outline-danger" role="button" onclick="cancelHor(<?php echo $datos['id_pacientes']; ?>)">
-                                                    <i class="bi bi-x-lg text-danger"></i>
+                                                <a class="btn btn-outline-danger" onclick="deletePac(<?php echo $datos['id_pacientes']; ?>)" role="button">
+                                                    <i class="bi bi-trash-fill text-danger"></i><span class="text-danger"> Eliminar</span>
                                                 </a>
                                             </center>
                                         </td>
@@ -93,7 +95,35 @@ if (!isset($_SESSION['us'])) {
             }
         });
     });
+
+    function deletePac(idPac) {
+        
+        fetch("../model/borrarPaciente.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                idPac: idPac
+            })
+        })
+            .then(response => response.text())
+            .then(respuesta => {
+                if (respuesta == true) {
+                    Swal.fire({
+                        title: "Eliminado",
+                        text: "Paciente eliminado correctamente",
+                        icon: "success",
+                        showConfirmButton: false,
+                        timer: 1500,
+                    }).then(function() {
+                        window.location.href;
+                    });
+                }
+            });
+    }
 </script>
+<script src="js/tableClick.js"></script>
 <?php
 include("../../../templates/footer.php");
 ?>
