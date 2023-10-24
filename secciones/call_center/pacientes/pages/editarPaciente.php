44bc402ac980acbbfb5fb74a8141105512a49aba
Alex Venegas <alfredo.alexis30@gmail.com>
2023-10-24 10:24:08 -0600

    Validacion de segundo telefono completada

--- secciones/call_center/pacientes/pages/editarPaciente.php
+++ secciones/call_center/pacientes/pages/editarPaciente.php
@@ -83,12 +83,12 @@ if (!isset($_SESSION['us'])) {
                             <p id="errTelUno" style="color:red; font-weight:bold;"></p>
                         </div>
 
-                        <div class="contenido col-md-1" style="display: flex;" id="add">
+                        <div class="contenido col-md-1" <?php echo ($pac['telefonoDos'] != '' ? 'style="display: none;"' : 'style="display: flex;"'); ?> id="add">
                             <span class="badge bg-primary fs-4" id="addBoton">+</span>
                         </div>
-                        <div class="contenido col-md-3" style="display: none;" id="tel">
+                        <div class="contenido col-md-3" <?php echo ($pac['telefonoDos'] != '' ? 'style="display: block;"' : 'style="display: none;"'); ?> id="tel">
                             <label for="telDos" class="form-label">Teléfono 2:</label>
-                            <input type="text" maxlength="10" class="form-control" name="telDos" id="telDos" placeholder="Ingresa un número de teléfono">
+                            <input type="text" maxlength="10" class="form-control" name="telDos" id="telDos" placeholder="Ingresa un número de teléfono" value="<?php echo $pac['telefonoDos']; ?>">
                             <span class="badge bg-danger border border-light rounded-circle" id="delBoton">X</span>
                             <p id="errTelDos" style="color:red; font-weight:bold;"></p>
                         </div>
