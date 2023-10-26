b8ce5af465a01e1f20adddc2e6f20cb79a146b91
Oscar <oslu3132@gmail.com>
2023-10-26 13:27:03 -0600

    cabio arribo muy sensual, que digo muy central

--- secciones/call_center/arribo/php/cargarArribo.php
+++ secciones/call_center/arribo/php/cargarArribo.php
@@ -1,8 +1,8 @@
 <?php
 include '../../../../connection/conexion.php';
 
-$sql = "SELECT a.id_sv, a.num_paciente, p.Nombres, p.Apellidos, a.fecha, a.hora, a.moti_consulta, a.num_medico, u.Usuario, a.estado 
-      FROM asignacion_servicio a INNER JOIN usuarios u, pacientes_oxigeno p 
+$sql = "SELECT a.id_sv, a.num_paciente, p.nombres, p.apellidos, a.fecha, a.hora, a.moti_consulta, a.num_medico, u.Nombres, a.asignacion 
+      FROM asignacion_servicio a INNER JOIN usuarios u, pacientes_call_center p 
       WHERE p.id_pacientes = a.num_paciente AND a.num_medico = U.id_usuarios;";
 
 $stat = $con->prepare("$sql");
@@ -28,31 +28,31 @@ $resultados = $stat->fetchAll(PDO::FETCH_ASSOC);
         <tbody>
             <?php
             foreach ($resultados as $filas) {
-                if ($filas['estado'] == 1 || $filas['estado'] == 2) {
+                if ($filas['asignacion'] == 1 || $filas['asignacion'] == 2) {
                     echo '<tr>
                             <td><b>' . $filas['id_sv'] . '</b></td>
-                            <td>' . $filas['Usuario'] . '</td>
-                            <td>' . $filas['Nombres'] . ' ' . $filas['Apellidos'] . '</td>
-                            <td>' . $filas['estado'] . '</td>
+                            <td>' . $filas['Nombres'] . '</td>
+                            <td>' . $filas['nombres'] . ' ' . $filas['apellidos'] . '</td>
+                            <td>' . $filas['asignacion'] . '</td>
                             <td>' . $filas['hora'] . '</td>
                           <tr/>';
                 }
-            } 
+            }
             ?>
         </tbody>
     </table>
-            <br>
+    <br>
     <?php
-    $sql = "SELECT a.id_sv, a.num_paciente, p.Nombres, p.Apellidos, a.fecha, a.hora, a.moti_consulta, a.num_medico, u.Usuario, a.estado 
-            FROM asignacion_servicio a INNER JOIN usuarios u, pacientes_oxigeno p 
-            WHERE p.id_pacientes = a.num_paciente AND a.num_medico = U.id_usuarios;";
+    $sql = "SELECT a.id_sv, a.num_paciente, p.nombres, p.apellidos, a.fecha, a.hora, a.moti_consulta, a.num_medico, u.Nombres, a.asignacion 
+FROM asignacion_servicio a INNER JOIN usuarios u, pacientes_call_center p 
+WHERE p.id_pacientes = a.num_paciente AND a.num_medico = U.id_usuarios;";
 
     $stat = $con->prepare("$sql");
     $stat->execute();
     $resultados = $stat->fetchAll(PDO::FETCH_ASSOC);
 
     ?>
-        <table>
+    <table>
         <thead>
             <tr class="historial">
                 <th colspan="5">historial</th>
@@ -68,14 +68,14 @@ $resultados = $stat->fetchAll(PDO::FETCH_ASSOC);
         <tbody>
             <?php
             foreach ($resultados as $filas) {
-                if ($filas['estado'] == 3) {
+                if ($filas['asignacion'] == 3) {
                     echo '<tr>
-                            <td><b>' . $filas['id_sv'] . '</b></td>
-                            <td>' . $filas['Usuario'] . '</td>
-                            <td>' . $filas['Nombres'] . ' ' . $filas['Apellidos'] . '</td>
-                            <td>' . $filas['estado'] . '</td>
-                            <td>' . $filas['hora'] . '</td>
-                          <tr/>';
+                    <td><b>' . $filas['id_sv'] . '</b></td>
+                    <td>' . $filas['Nombres'] . '</td>
+                    <td>' . $filas['nombres'] . ' ' . $filas['apellidos'] . '</td>
+                    <td>' . $filas['asignacion'] . '</td>
+                    <td>' . $filas['hora'] . '</td>
+                  <tr/>';
                 }
             }
             ?>
