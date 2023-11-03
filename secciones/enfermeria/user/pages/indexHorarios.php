0b0d0d1a5b3cce0e18a5ac3f46f672398492466b
Alex Venegas <alfredo.alexis30@gmail.com>
2023-11-03 15:10:04 -0600

    Update interfaz enfermeros
    Se actualizaron modelos para apuntar a la tabla pacientes_call_center. Además de que se hicieron modificaciones en la sección de pacientes y horarios. Queda pendiente la asistencia con la API de Maps.

--- secciones/enfermeria/user/pages/indexHorarios.php
+++ secciones/enfermeria/user/pages/indexHorarios.php
@@ -29,14 +29,40 @@ if (!isset($_SESSION['us'])) {
     <div class="row">
         <div class="card">
             <div class="card-body">
-
+                <table class="table table-bordered  border-dark table-hover" id="myTable">
+                    <thead class="table-dark">
+                        <tr class="table-active table-group-divider" style="text-align: center;">
+                            <th>Paciente</th>
+                            <th>Horario</th>
+                            <th>Fecha</th>
+                            <th>Estado</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody id="tableCont">
+                    </tbody>
+                </table>
+                <input type="hidden" id="idUs" value="<?php echo $_SESSION['idus']; ?>">
             </div>
         </div>
     </div>
 </main>
 
 </html>
+<script>
+    $(document).ready(function() {
+        $.noConflict();
+        $('#myTable').DataTable({
+            "language": {
+                "url": "https://cdn.datatables.net/plug-ins/1.10.21/i18n/Spanish.json"
+            },
+            "order": [],
+        });
+    });
+</script>
+<script src="../js/indexHorario.js"></script>
+<script src="../js/statusHorario.js"></script>
 
 <?php
-    include("../../../../templates/footer.php");
+include("../../../../templates/footer.php");
 ?>
