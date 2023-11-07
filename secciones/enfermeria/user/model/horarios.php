68252e98b76f07f20b5f78d9b17560d6ccf087e1
Alex Venegas <alfredo.alexis30@gmail.com>
2023-11-07 10:02:57 -0600

    Update sección horarios
    Se actualiza el index de horarios con el status y los botones. Por cambiar botones y colores.

--- secciones/enfermeria/user/model/horarios.php
+++ secciones/enfermeria/user/model/horarios.php
@@ -9,7 +9,7 @@ if(isset($data['id']) && $data['id'] != '' && $data['id'] != NULL) {
         AND u.id_usuarios = a.id_usuario
         AND p.id_pacientes = a.id_pacienteEnfermeria
         AND t.id_tipoServicio = a.id_tipoServicio
-        ORDER BY a.fecha ASC;"
+        ORDER BY a.fecha ASC, a.horarioEntrada ASC, a.horarioSalida ASC"
     );
     $iduser=$data['id'];
     $sentenciaHora->bindParam(':idUser', $iduser);
