4963a1471be8e2cc3e608d0c4bc4aa598d059ec0
Alex Venegas <alfredo.alexis30@gmail.com>
2023-11-08 07:52:32 -0600

    Upp index asistencias
    Se colocó la foto de la base de datos y se asignó el lugar para el maps embed

--- secciones/enfermeria/user/pages/indexAsistencias.php
+++ secciones/enfermeria/user/pages/indexAsistencias.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION['us'])) {
 } elseif (isset($_SESSION['us'])) {
     include("../../../../templates/header.php");
     include("../../../../connection/conexion.php");
-    include("../model/horarios.php");
+    include("../model/asistencias.php");
 } else {
     echo "Error en el sistema";
 }
@@ -18,48 +18,58 @@ if (!isset($_SESSION['us'])) {
     <link rel="stylesheet" href="<?php echo $url_base; ?>assets/css/edit.css">
 </head>
 <main id="main" class="main">
-    <div class="pagetitle">
-        <h1>Asistencias</h1>
-        <br>
-        <nav>
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="<?php echo $url_base; ?>secciones/enfermeria/user/index.php">Dashboard</a></li>
-                <li class="breadcrumb-item active">Asistencias</li>
-            </ol>
-        </nav>
-    </div>
-    <section class="section dashboard">
-        <div class="row">
-            <div class="col-lg-7">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Check In</h5>
-                        <div class="row align-items-center">
-                            <img src="" alt="">
-                        </div>
-                                                
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-5">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Datos Generales</h5>
-                        <dl class="row">
-                            <dt class="col-lg-3 col-md-4 label">Paciente:</dt>
-                            <dd class="col-lg-9 col-md-8" id="paciente">asdasda</dd>
-                            <dt class="col-lg-3 col-md-4 label">Hora Check:</dt>
-                            <dd class="col-lg-9 col-md-8" id="horaEntrada">asdasda</dd>
-                            <dt class="col-lg-3 col-md-4 label">Fecha Check:</dt>
-                            <dd class="col-lg-9 col-md-8" id="fechaEntrada">asdasdas</dd>
-                        </dl>
-                    </div>
-                </div>
-            </div>
+    <?php foreach ($lista_asistencias as $asis) { ?>
+        <div class="pagetitle">
+            <h1>Asistencias</h1>
+            <br>
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="<?php echo $url_base; ?>secciones/enfermeria/user/index.php">Dashboard</a></li>
+                    <li class="breadcrumb-item active">Asistencias</li>
+                </ol>
+            </nav>
         </div>
-    </section>
+        <section class="section dashboard">
+            <div class="row">
+                <div class="col-lg-7">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Check In</h5>
+                            <div class="row align-items-center">
+                                <img src="<?php echo $asis['checkFotografia']; ?>" alt="Foto de Asistencias">
+                            </div>
 
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Ubicación</h5>
+                            <iframe width="600" height="450" style="border:0" loading="lazy"
+                            allowfullscreen referrerpolicy="no-referrer-when-downgrade"
+                            src="https://www.google.com/maps/embed/v1/place?key=API_KEY&q=Space+Needle,Seattle+WA">
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Datos Generales</h5>
+                            <dl class="row">
+                                <dt class="col-lg-3 col-md-4 label">Paciente:</dt>
+                                <dd class="col-lg-9 col-md-8" id="paciente"><?php echo $asis['id_empleadoEnfermeria']; ?></dd>
+                                <dt class="col-lg-3 col-md-4 label">Hora Check:</dt>
+                                <dd class="col-lg-9 col-md-8" id="horaEntrada"><?php echo $asis['checkTime']; ?></dd>
+                                <dt class="col-lg-3 col-md-4 label">Fecha Check:</dt>
+                                <dd class="col-lg-9 col-md-8" id="fechaEntrada"><?php echo $asis['fechaAsis']; ?></dd>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <?php } ?>
 </main>
 
 </html>
