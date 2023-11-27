f63ef916e13a7442179c5d9b39889c374856152e
Alex Venegas <alfredo.alexis30@gmail.com>
2023-11-27 15:04:31 -0600

    Update lista de horarios

--- secciones/enfermeria/user/js/indexHorario.js
+++ secciones/enfermeria/user/js/indexHorario.js
@@ -13,7 +13,10 @@ function getDatos() {
         //Creación de las partes de la Card
         var card = document.createElement("div"),
           cardHead = document.createElement("div"),
-          cardBody = document.createElement("div");
+          cardBody = document.createElement("div"),
+          list = document.createElement("dl");
+        //Clase de la List
+        list.classList.add("row");
         //Clase de la Card
         card.classList.add("card");
         //Clases y atributos del card header
@@ -29,30 +32,55 @@ function getDatos() {
         cardBody.id = "card" + dato.id_asignacionHorarios;
         //Datos dentro de los card header y card body
         cardHead.innerHTML =
-          "<div class='row justify-content-between d-flex'><div class='col-10'><b class='title-hour'>" +
+          "<div class='row justify-content-between d-flex'>" +
+          "<div class='col-10'>" +
+          "<b class='title-hour'>" +
           dato.nomPaciente +
-          "</b></div><div class='col-2'><i class='bi bi-chevron-down ms-auto'></i></div></div>";
-        cardBody.innerHTML +=
-          "<div class='row justify-content-between d-flex'><dt class='col-lg-3 col-md-4 label'>Fecha:</dt><dd class='col-lg-9 col-md-8>" +
+          "</b>" +
+          "</div>" +
+          "<div class='col-2'>" +
+          "<i class='bi bi-chevron-down ms-auto'></i>" +
+          "</div>" +
+          "</div>";
+        list.innerHTML +=
+          "<dt class='col-4'>" +
+          "Fecha:" +
+          "</dt>" +
+          "<dd class='col-8'>" +
           dato.fecha +
-          "</dd><dt class='col-lg-3 col-md-4 label'>Hora entrada:</dt><dd class='col-lg-9 col-md-8>" +
+          "</dd>" +
+          "<dt class='col-4'>" +
+          "Hora entrada:" +
+          "</dt>" +
+          "<dd class='col-8'>" +
           dato.horarioEntrada +
-          "</dd></div>";
-        cardBody.innerHTML += "";
-        cardBody.innerHTML += "";
-        cardBody.innerHTML += "";
-        cardBody.innerHTML += "";
-        cardBody.innerHTML +=
-          "<dt class='col-lg-3 col-md-4 label'>Hora salida:</dt>";
-        cardBody.innerHTML +=
-          "<dd class='col-lg-9 col-md-8>" + dato.horarioSalida + "</dd>";
-        cardBody.innerHTML +=
-          "<dt class='col-lg-3 col-md-4 label'>Servicio:</dt>";
-        cardBody.innerHTML +=
-          "<dd class='col-lg-9 col-md-8>" + dato.nomGuardia + "</dd>";
-        cardBody.innerHTML += "</dl>";
-
+          "</dd>" +
+          "<dt class='col-4'>" +
+          "Hora salida:" +
+          "</dt>" +
+          "<dd class='col-8'>" +
+          dato.horarioSalida +
+          "</dd>" +
+          "<dt class='col-4'>" +
+          "Servicio:" +
+          "</dt>" +
+          "<dd class='col-8'>" +
+          dato.nomGuardia +
+          "</dd>" +
+          "<dt class='col-4'>" +
+          "Estado:" +
+          "</dt>" +
+          "<dd class='col-8' id='estado" +
+          dato.id_asignacionHorarios +
+          "'>" +
+          "<span class='badge' id='badge" +
+          dato.id_asignacionHorarios +
+          "'>" +
+          dato.estado +
+          "</span>" +
+          "</dd>";
         //Se insertan dentro del Card sus partes
+        cardBody.appendChild(list);
         card.appendChild(cardHead);
         card.appendChild(cardBody);
         //Se añade en el main antes de la etiqueta oculta con el id del usuario
@@ -60,4 +88,20 @@ function getDatos() {
       });
     });
 }
+function getStatus() {
+  var idus = document.getElementById("idus");
+  fetch("../model/statusHorario.php", {
+    method: "POST",
+    body: JSON.stringify({ id: idus.value }),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then((datos) => {
+      console.log(datos);
+      datos.forEach((dato) => {
+        console.log(dato);
+      });
+    });
+}
 getDatos();
+getStatus();
