580f3af55f36749ec0c9f9539ed8229585752c74
oscar <ofter3132@gmail.com>
2024-01-08 12:31:39 -0600

    casí listo hasta  ahora :P

--- secciones/call_center/servicios/crearServicio.php
+++ secciones/call_center/servicios/crearServicio.php
@@ -23,19 +23,18 @@ if (!isset($_SESSION['us'])) {
     <section class="section dashboard">
         <div class="card">
             <div class="card-header" style="border: 2px solid #005880; background: #005880;">
-                <h4
-                    style="text-align: center; color: #fff; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
-                    Datos del nuevo servicio</H4>
+                <h4 style="text-align: center; color: #fff; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
+                    Datos del nuevo servicio
+                </h4>
             </div>
-            <div class="card-body" style="border: 12px solid #005880;">
+            <div class="card-body" style="border: 2px solid #BFE5FF;">
                 <form action="./model/agregarServicio.php" method="POST" class="formLogin row g-3" id="formulario">
 
                     <div class="contenido col-md-6"> <br>
                         <div class="formulario__grupo" id="grupo__nomServicio">
                             <label for="nomServicio" class="form-label">Nombre del servicio</label>
                             <div class="formulario__grupo-input">
-                                <input type="text" class="form-control" name="nomServicio" id="nomServicio"
-                                    placeholder="Eje: VISITA MEDICA DOMICILIARIA">
+                                <input type="text" class="form-control" name="nomServicio" id="nomServicio" placeholder="Eje: VISITA MEDICA DOMICILIARIA">
                             </div>
                         </div>
                     </div>
@@ -44,8 +43,7 @@ if (!isset($_SESSION['us'])) {
                         <div class="formulario__grupo" id="grupo__descripServicio">
                             <label for="descripServicio" class="form-label">Descripcion del servicio</label>
                             <div class="formulario__grupo-input">
-                                <input type="text" class="form-control" name="descripServicio" id="descripServicio"
-                                    placeholder="Eje: horario de 07:00 a las 20:00 hrs. ">
+                                <input type="text" class="form-control" name="descripServicio" id="descripServicio" placeholder="Eje: horario de 07:00 a las 20:00 hrs. ">
                             </div>
                         </div>
                     </div>
@@ -53,52 +51,51 @@ if (!isset($_SESSION['us'])) {
                     <div class="col-12">
                         <div class="formulario__grupo formulario__grupo-btn-enviar">
                             <button type="submit" class="btn btn-outline-success">Guardar</button>
-                            <a role="button" onclick="confirmCancel(event)" name="cancelar"
-                                class="btn btn-outline-danger"> Cancelar</a>
+                            <a role="button" onclick="confirmCancel(event)" name="cancelar" class="btn btn-outline-danger"> Cancelar</a>
                         </div>
                 </form>
             </div>
         </div>
 </main>
 <script>
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.formLogin').addEventListener('submit', function(event) {
-        event.preventDefault();
+    document.addEventListener('DOMContentLoaded', function() {
+        document.querySelector('.formLogin').addEventListener('submit', function(event) {
+            event.preventDefault();
 
-        // Verifica si los campos obligatorios están vacíos
-        var nomServicio = document.getElementById('nomServicio').value;
+            // Verifica si los campos obligatorios están vacíos
+            var nomServicio = document.getElementById('nomServicio').value;
 
 
-        if (!nomServicio) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Campos vacíos',
-                text: 'Por favor, completa todos los campos obligatorios.',
-            });
-        } else {
-            this.submit();
-        }
+            if (!nomServicio) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Campos vacíos',
+                    text: 'Por favor, completa todos los campos obligatorios.',
+                });
+            } else {
+                this.submit();
+            }
+        });
     });
-});
 </script>
 <script>
-function confirmCancel(event) {
-    event.preventDefault();
-    Swal.fire({
-        title: '¿Estás seguro?',
-        text: "Si cancelas, se perderán los datos ingresados.",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Sí, cancelar',
-        cancelButtonText: 'No, continuar'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            window.location.href = "<?php echo $url_base; ?>secciones/call_center/servicios/index.php";
-        }
-    });
-}
+    function confirmCancel(event) {
+        event.preventDefault();
+        Swal.fire({
+            title: '¿Estás seguro?',
+            text: "Si cancelas, se perderán los datos ingresados.",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, cancelar',
+            cancelButtonText: 'No, continuar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = "<?php echo $url_base; ?>secciones/call_center/servicios/index.php";
+            }
+        });
+    }
 </script>
 <?php
 include("../../../templates/footer.php");
