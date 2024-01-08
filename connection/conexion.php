19b267cdd3f48fde588263be625f8b6ae8acf935
DevOxilive <sistemas@oxilive.com.mx>
2024-01-08 16:02:34 -0600

    Ya esta todo listo

--- connection/conexion.php
+++ connection/conexion.php
@@ -1,13 +1,14 @@
 <?php
 
+// $servidor = "localhost";
+// $baseDeDatos = "u199109938_bdoxilive";
+// $usuario = "u199109938_administrator";
+// $clave = "sudo-Sw03j3j3j3";
 $servidor = "localhost";
-$baseDeDatos = "u199109938_bdoxilive";
-$usuario = "u199109938_administrator";
-$clave = "sudo-Sw03j3j3j3";
-//$servidor = "localhost";
-//$baseDeDatos = "bdoxilive";
-//$usuario = "root";
-//$clave = "";
+// $baseDeDatos = "bdoxilive";
+$baseDeDatos = "u199109938_swoe";
+$usuario = "root";
+$clave = "";
 
 try {
     $con = new PDO("mysql:host=$servidor;dbname=$baseDeDatos", $usuario, $clave);
