f4745eeb7fea9236751c0359d37c709f77d9f5b5
DevOxilive <sistemas.oxilive@gmail.com.mx>
2024-01-26 08:11:54 -0600

    Sin miedo al exito

--- secciones/Capital_humano/empleados/crear.php
+++ secciones/Capital_humano/empleados/crear.php
@@ -66,68 +66,71 @@ if (!isset($_SESSION['us'])) {
                         </select>
                     </div>
                     <div class="contenido col-md-4"> <br>
-                        <label for="usuario" class="form-label">Cuenta con contrato</label>
-                        <input type="text" class="form-control" name="contrato" id="contrato" maxlength="2"
-                            placeholder="Escriba SI / NO" required>
-                        <div class="valid-feedback">
-                            contrato correcto
-                        </div>
+                        <label for="contrato" class="form-label">Cuenta con contrato</label>
+                        <select name="contrato" id="contrato" class="form-select" required >
+                            <option value="">Seleccione contrato</option>
+                            <option value="SI">Si contratado</option>
+                            <option value="NO">No contratado</option>
+                        </select>
                     </div>
                     
                     <hr>
                     <h1 style="text-align: center;">Datos Generales</h1>
-                    
-                        <div class="contenido col-md-3 position-relative"> <br>
-                            <label for="nombres" class="form-label">Nombre:(s)</label>
-                            <input type="text" class="form-control" name="nombres" id="nombres"
-                                placeholder="Ingrese el nombre" maxlength="20" required>
-                            <div class="valid-tooltip">
-                                Nombre apropiado
-                            </div>
-                        </div>
-                        <div class="contenido col-md-3 position-relative"> <br>
-                            <label for="apellidos" class="form-label">Apellido:(s)</label>
-                            <input type="text" class="form-control" name="apellidos" id="apellidos"
-                                placeholder="Apellidos" maxlength="20" required>
-                            <div class="valid-tooltip">
-                                Apllido apropiado
-                            </div>
-                        </div>
-                        <div class="contenido col-md-3"> <br>
-                            <label for="genero" class="form-label">Género</label>
-                            <select id="genero" name="genero" class="form-select">
-                                <?php foreach ($lista_genero as $genero) { ?>
-                                <option value="<?php echo $genero['id_genero']; ?>">
-                                    <?php echo $genero['genero']; ?>
-                                </option>
-                                <?php } ?>
-                            </select>
 
+                    <div class="contenido col-md-3 position-relative"> <br>
+                        <label for="nombres" class="form-label">Nombre:(s)</label>
+                        <input type="text" class="form-control" name="nombres" id="nombres"
+                            placeholder="Ingrese el nombre" maxlength="20" required>
+                        <div class="valid-tooltip">
+                            Nombre apropiado
                         </div>
-                        <div class="contenido col-md-3 position-relative"> <br>
-                            <label for="curp" class="form-label">CURP</label>
-                            <input type="text" class="form-control" name="curp" id="curp" placeholder="curp"
-                                maxlength="30" required>
-                            <div class="valid-tooltip">
-                                CURP correcto
-                            </div>
+                    </div>
+                    <div class="contenido col-md-3 position-relative"> <br>
+                        <label for="apellidos" class="form-label">Apellido:(s)</label>
+                        <input type="text" class="form-control" name="apellidos" id="apellidos" placeholder="Apellidos"
+                            maxlength="20" required>
+                        <div class="valid-tooltip">
+                            Apllido apropiado
                         </div>
-                        <div class="contenido col-md-3">
+                    </div>
+                    <div class="contenido col-md-3"> <br>
+                        <label for="genero" class="form-label">Género</label>
+                        <select id="genero" name="genero" class="form-select">
+                            <?php foreach ($lista_genero as $genero) { ?>
+                            <option value="<?php echo $genero['id_genero']; ?>">
+                                <?php echo $genero['genero']; ?>
+                            </option>
+                            <?php } ?>
+                        </select>
+
+                    </div>
+                    <div class="contenido col-md-3 position-relative"> <br>
+                        <label for="curp" class="form-label">CURP</label>
+                        <input type="text" class="form-control" name="curp" id="curp" placeholder="curp" maxlength="30"
+                            required>
+                        <div class="valid-tooltip">
+                            CURP correcto
+                        </div>
+                    </div>
+                    <div class="contenido col-md-3">
                         <label for="rfc" class="form-label">RFC</label>
                         <input type="text" class="form-control" name="rfc" id="rfc" maxlength="15"
                             placeholder="x1x1x1x1x1x1x1x" required>
                         <div class="valid-feedback">
                             RFC correcto
                         </div>
-                    
+
                     </div>
-                    <div class="contenido col-md-3 position-relative">
+                    <div class="contenido col-md-3">
                         <label for="telefono" class="form-label">Telefono</label>
                         <input type="Phone" class="form-control" name="telefono" id="telefono"
                             placeholder="Telefono de 10 digitos" required>
-                        <div class="valid-tooltip">
-                            Telefono Correcto
-                        </div>
+                    </div>
+                    <div class="contenido col-md-3">
+                        <label for="cuentaInput" class="form-label">num.Cuenta</label>
+                        <input type="text" class="form-control" name="cuentaInput" id="cuentaInput"
+                            oninput="validarCuenta()" maxlength="20" placeholder="12345432" required>
+                        <div id="mensajeError" class="error-message"></div>
                     </div>
                     <div class="contenido col-md-3 position-relative">
                         <label for="email" class="form-label">Correo</label>
@@ -137,6 +140,21 @@ if (!isset($_SESSION['us'])) {
                             Opcional
                         </div>
                     </div>
+                    <div class="contenido col-md-3 position-relative">
+                        <label for="nss" class="form-label">Numero de Seguro Social</label>
+                        <input type="text" class="form-control" name="nss" id="nss" placeholder="Ingrese el correo"
+                            maxlength="11">
+                    </div>
+                    <div class="contenido col-md-3">
+                        <label for="nivel_educativo" class="form-label">Nivel Estudios</label>
+                        <select name="nivel_educativo" id="nivel_educativo" class="form-select" required >
+                            <option value="">Seleccione Grado</option>
+                            <option value="Cédula">Cédula</option>
+                            <option value="Bachillerato">Bachillerato</option>
+                            <option value="Secundaria">Secundaria</option>
+                        </select>
+                    </div>
+                    
                     <hr>
                     <h1>Domicilio Actual </h1>
                     <!--Aquí voy a meter el codigo postal-->
@@ -161,16 +179,15 @@ if (!isset($_SESSION['us'])) {
                         <input type="text" class="form-control" name="numInt" id="numInt" maxlength="15"
                             placeholder="12">
                     </div>
-                
+
                     <div class="contenido col-md-2">
                         <label for="cp" class="form-label">Código Postal:</label>
-                        <input type="text" class="form-control" id="cp"
-                            placeholder="Ejem: 92734" required>
+                        <input type="text" class="form-control" id="cp" placeholder="Ejem: 92734" required>
                     </div>
                     <div class="contenido col-md-4">
-                        <label for="colonia" class="form-label" >Colonia:</label>
+                        <label for="colonia" class="form-label">Colonia:</label>
                         <select name="colonia" id="colonia" class="form-select" required>
-                            <option value="" >Selecciona un Código Postal</option>
+                            <option value="">Selecciona un Código Postal</option>
                         </select>
                     </div>
                     <div class="contenido col-md-3">
@@ -186,7 +203,7 @@ if (!isset($_SESSION['us'])) {
                         </select>
                     </div>
                     <!--Aquí lo voy a cerrar el codigo postal-->
-                    
+
 
                     <div class="contenido col-md-6 position-relative">
                         <label for="calleUno" class="form-label">Entre Calle:</label>
@@ -215,48 +232,114 @@ if (!isset($_SESSION['us'])) {
                     </div>
                     <hr>
                     <h1 style="text-align: center;">Comprobantes</h1>
-                    <div class="contenido col-md-4 position-relative">
-                        <label for="estudio" class="form-label">Estudio</label>
-                        <input type="file" class="formulario__input-file" name="estudio" id="estudio">
-                        <div class="optional-tooltip">
-                            Opcional
-                        </div>
+
+                    <div class="contenido col-md-4">
+                        <label for="certificadoEstudios" class="form-label">Ultimo Certificado / Cedula</label>
+                        <input type="file" class="form-control" aria-label="file example" required name="certificadoEstudios"
+                            id="certificadoEstudios">
+                        <div class="">Seleccione archivo pdf.</div>
                     </div>
 
                     <div class="contenido col-md-4">
-                        <label for="cuenta" class="form-label">Cuenta</label>
+                        <label for="cuenta" class="form-label">Comprobante de Cuenta</label>
                         <input type="file" class="form-control" aria-label="file example" required name="cuenta"
                             id="cuenta">
                         <div class="">Seleccione archivo pdf.</div>
                     </div>
+                                    <!--INES DEL EMPLEADO-->
+                    <div class="contenido col-md-4">
+                        <label for="ineAnverso" class="form-label">(INE) Credencial parte superior</label>
+                        <input type="file" class="form-control" aria-label="file example" required
+                            name="ineAnverso" id="ineAnverso">
+                    </div>
+                    <div class="contenido col-md-4">
+                        <label for="ineReverso" class="form-label">(INE) Credencial parte inferior</label>
+                        <input type="file" class="form-control" aria-label="file example" required
+                            name="ineReverso" id="ineReverso">
+                    </div>
 
                     <div class="contenido col-md-4">
-                        <label for="cedula" class="form-label">Ultimo Certificado / Cedula</label>
-                        <input type="file" class="form-control" aria-label="file example" required name="cedula"
-                            id="cedula">
-                        <div class="">Seleccione archivo pdf.</div>
+                        <label for="actaNacimiento" class="form-label">Acta De Nacimiento</label>
+                        <input type="file" class="form-control" aria-label="file example" required
+                            name="actaNacimiento" id="actaNacimiento">
                     </div>
 
                     <div class="contenido col-md-4">
-                        <label for="referencias" class="form-label">Comprobante de domicilio</label>
+                        <label for="comprobanteDomicilio" class="form-label">Comprobante De Domicilio</label>
                         <input type="file" class="form-control" aria-label="file example" required
-                            name="comprobante_domicilio" id="comprobante_domicilio">
-                        <div class="">Seleccione archivo pdf.</div>
+                            name="comprobanteDomicilio" id="comprobanteDomicilio">
                     </div>
                     <div class="contenido col-md-4">
-                        <label for="credencialFrente" class="form-label">(INE) Credencial parte superior</label>
+                        <label for="nssDoc" class="form-label">Nùmero De Seguro Social</label>
                         <input type="file" class="form-control" aria-label="file example" required
-                            name="credencialFrente" id="credencialFrente">
+                            name="nssDoc" id="nssDoc">
+                    </div>
+
+                    <div class="contenido col-md-4">
+                        <label for="curpDoc" class="form-label">CURP</label>
+                        <input type="file" class="form-control" aria-label="file example" required
+                            name="curpDoc" id="curpDoc">
+                    </div>
+
+                    <div class="contenido col-md-4">
+                        <label for="rfcDoc" class="form-label">RFC</label>
+                        <input type="file" class="form-control" aria-label="file example" required
+                            name="rfcDoc" id="rfcDoc">
+                    </div>
+                    <!--Estos tambien hay que insertarlos-->
+                    <div class="contenido col-md-4">
+                        <label for="referenciaLabUno" class="form-label">Referencia Laboral</label>
+                        <input type="file" class="form-control" aria-label="file example"
+                            name="referenciaLabUno" id="referenciaLabUno">
+                            <div class="optional-tooltip">
+                            Opcional
+                        </div>
+                        </div>
+                    
+                        <div class="contenido col-md-4">
+                        <label for="referenciaLabDos" class="form-label">Referencia Laboral</label>
+                        <input type="file" class="form-control" aria-label="file example"
+                            name="referenciaLabDos" id="referenciaLabDos">
+                            <div class="optional-tooltip">
+                            Opcional
+                        </div>
                     </div>
                     <div class="contenido col-md-4">
-                        <label for="credencialAtras" class="form-label">(INE) Credencial parte inferior</label>
-                        <input type="file" class="form-control" aria-label="file example" required
-                            name="credencialAtras" id="credencialAtras">
+                        <label for="estudioSocio" class="form-label">Estudio Socio Economico</label>
+                        <input type="file" class="form-control" aria-label="file example"
+                            name="estudioSocio" id="estudioSocio">
+                            <div class="optional-tooltip">
+                            Opcional
+                        </div>
                     </div>
                     <div class="contenido col-md-4">
-                        <label for="nss" class="form-label">No. Seguro Social</label>
-                        <input type="file" class="formulario__input-file" name="nss" id="nss">
+                        <label for="licenciaUno" class="form-label">Licencia: (1)</label>
+                        <input type="file" class="form-control" aria-label="file example"
+                            name="licenciaUno" id="licenciaUno">
+                            <div class="optional-tooltip">
+                            Opcional
+                        </div>
                     </div>
+                    <div class="contenido col-md-4">
+                        <label for="licenciaDos" class="form-label">Licencia: (2)</label>
+                        <input type="file" class="form-control" aria-label="file example"
+                            name="licenciaDos" id="licenciaDos">
+                            <div class="optional-tooltip">
+                            Opcional
+                        </div>
+                    </div>
+                    <div class="contenido col-md-4">
+                        <label for="licenciaTres" class="form-label">Licencia: (3)</label>
+                        <input type="file" class="form-control" aria-label="file example"
+                            name="licenciaTres" id="licenciaTres">
+                            <div class="optional-tooltip">
+                            Opcional
+                        </div>
+                    </div>
+                    
+
+
+                    
                     <div class="col-12">
                         <button type="submit" class="btn btn-outline-primary">Guardar</button>
                         <a role="button" onclick="confirmCancel(event)" name="cancelar" class="btn btn-outline-danger">
