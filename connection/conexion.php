f3d2674bcad0d7f29f15dff7182cb3f8b186aa23
oscar <ofter3132@gmail.com>
2024-02-22 15:09:38 -0600

    para hostinger

--- connection/conexion.php
+++ connection/conexion.php
@@ -1,13 +1,12 @@
 <?php
-// $servidor = "localhost";
-// $baseDeDatos = "u199109938_bdoxilive";
-// $usuario = "u199109938_administrator";
-// $clave = "sudo-Sw03j3j3j3";
 $servidor = "localhost";
 $baseDeDatos = "bdoxilive";
 $usuario = "root";
 $clave = "";
-
+$servidor = "localhost";
+$baseDeDatos = "u199109938_bdoxilive";
+$usuario = "u199109938_administrator";
+$clave = "sudo-Sw03j3j3j3";
 try {
     $con = new PDO("mysql:host=$servidor;dbname=$baseDeDatos", $usuario, $clave);
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
