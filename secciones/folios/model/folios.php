eeff59fac8c8573606f5e6267e934234073cc399
oscar <ofter3132@gmail.com>
2024-04-12 09:56:07 -0600

    estilizacion de folios. entrada folios.php

--- secciones/folios/model/folios.php
+++ secciones/folios/model/folios.php
@@ -24,86 +24,95 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $tipo = $_POST['tipo'];
                 $limit = $_POST['cant'];
             ?>
-                <b>Caracteristicas del folio(s)</b> solicitado: <br>
+                <div class="row mx-4">
+                    <div class="col-md-6">
+                        <b>Caracteristicas del folio(s)</b> solicitado: <br>
 
-                <?php
-                echo "Banco: " . $bancos . "<br>";
-                echo "Administradora: " . $administradora . "<br>";
-                echo "Tipo: " . $tipo . "<br>";
-                echo "Cantidad: " . $limit . "<br>";
-
-                if ($bancos == "" || $administradora == "" || $tipo == "" || $limit == "") {
-                    echo "faltan campos por seleccionar";
-                } else {
-                    $sql = 'SELECT * FROM folios 
+                        <?php
+                        echo "Banco: " . $bancos . "<br>";
+                        echo "Administradora: " . $administradora . "<br>";
+                        echo "Tipo: " . $tipo . "<br>";
+                        echo "Cantidad: " . $limit . "<br>";
+                        ?>
+                    </div>
+                    <div class="col-md-6 ms-6">
+                        <?php
+                        if ($bancos == "" || $administradora == "" || $tipo == "" || $limit == "") {
+                            echo "faltan campos por seleccionar";
+                        } else {
+                            $sql = 'SELECT * FROM folios 
                     WHERE bancoFolio = :bancoFolio
                     AND adminFolio = :adminFolio
                     AND tipo = :tipo
                     AND estado = 3';
-                    $buscar = $con->prepare($sql);
-                    $buscar->bindParam(':bancoFolio', $bancos);
-                    $buscar->bindParam(':adminFolio', $administradora);
-                    $buscar->bindParam(':tipo', $tipo);
-                    $buscar->execute();
-                    $folioSeek = $buscar->fetchAll(PDO::FETCH_ASSOC);
-                    $rows = $buscar->rowCount();
+                            $buscar = $con->prepare($sql);
+                            $buscar->bindParam(':bancoFolio', $bancos);
+                            $buscar->bindParam(':adminFolio', $administradora);
+                            $buscar->bindParam(':tipo', $tipo);
+                            $buscar->execute();
+                            $folioSeek = $buscar->fetchAll(PDO::FETCH_ASSOC);
+                            $rows = $buscar->rowCount();
 
-                    $cont = 0;
-                    $separar = 0;
-                    if (count($folioSeek) > 0) {
-                        if ($limit <= $rows) {
-                            foreach ($folioSeek as $fol) {
-                                $cont++;
-                                if ($cont <= $limit) {
-                                    $separar++;
-                ?>
-                                    <label for="folios"><?php echo $fol['folio'] ?></label>
-                                    <input type="checkbox" name="folios[]" id="" value="<?php echo $fol['id_folio'] ?>" checked>
-                            <?php
-                                    if ($separar >= 5) {
-                                        echo "<br>";
-                                        $separar = 0;
-                                    }
-                                }
-                            }
-                        } else {
-                            ?>
-                            <script>
-                                swal.fire({
-                                    title: '¡Ooups!',
-                                    text: "<?php echo "No hay '{$limit}': folios disponibles. Hay: '{$rows}' folios disponibles."; ?>",
-                                    icon: 'info'
-                                });
-                            </script>
-                        <?php
-                        }
-                    } else {
+                            $cont = 0;
+                            $separar = 0;
+                            if (count($folioSeek) > 0) {
+                                if ($limit <= $rows) {
+                                    foreach ($folioSeek as $fol) {
+                                        $cont++;
+                                        if ($cont <= $limit) {
+                                            $separar++;
                         ?>
-                        <script>
-                            swal.fire({
-                                title: '¡Ooups!',
-                                text: "<?php echo "No hay folios disponibles para el banco: {$bancos} y administradora: {$administradora}."; ?>",
-                                icon: 'info'
-                            });
-                        </script>
-                <?php
-                    }
-                }
-            } else {
-                ?>
+                                            <label for="folios"><?php echo $fol['folio'] ?></label>
+                                            <input type="checkbox" name="folios[]" id="" value="<?php echo $fol['id_folio'] ?>" checked>
+                                    <?php
+                                            if ($separar >= 5) {
+                                                echo "<br>";
+                                                $separar = 0;
+                                            }
+                                        }
+                                    }
+                                    ?>
+                    </div>
+                </div>
+            <?php
+                                } else {
+            ?>
                 <script>
                     swal.fire({
-                        icon: 'info',
-                        title: 'Recordatorio!',
-                        text: 'solo se pueden enviar un maximo de 50 folios.'
+                        title: '¡Ooups!',
+                        text: "<?php echo "No hay '{$limit}': folios disponibles. Hay: '{$rows}' folios disponibles."; ?>",
+                        icon: 'info'
                     });
                 </script>
             <?php
-                echo "Disminulle la cantidad de folios solicitados a un maximo de 50!.";
-            }
-        } else {
+                                }
+                            } else {
             ?>
-            <h4>No hay folios registrados en el sistema</h4>
+            <script>
+                swal.fire({
+                    title: '¡Ooups!',
+                    text: "<?php echo "No hay folios disponibles para el banco: {$bancos} y administradora: {$administradora}."; ?>",
+                    icon: 'info'
+                });
+            </script>
     <?php
-        }
-    }
+                            }
+                        }
+                    } else {
+    ?>
+    <script>
+        swal.fire({
+            icon: 'info',
+            title: 'Recordatorio!',
+            text: 'solo se pueden enviar un maximo de 50 folios.'
+        });
+    </script>
+<?php
+                        echo "Disminulle la cantidad de folios solicitados a un maximo de 50!.";
+                    }
+                } else {
+?>
+<h4>No hay folios registrados en el sistema</h4>
+<?php
+                }
+            }
